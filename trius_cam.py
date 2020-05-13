fd8d5ed394a8de4fecba2c83cf06995dd7ded251
Aidan <aidancgray@github.com>
2020-05-13 16:55:30 -0400

    Created file for Trius Cam Server

---
+++ trius_cam.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# trius_cam.py
+# 5/13/2020
+# Aidan Gray
+# aidan.gray@idg.jhu.edu
+#
+# This is a Python 3.8+ script for controlling
+# the Trius SX-694 Camera via TCP connection.
+
+import asyncio
+import socketserver
+
+
+
+if __name__ == "__main__":
+    print('debug')
