f527f95a02693a826559c94fcbd06b606cffccca
aidancgray <aidancgray@github.com>
2020-05-14 14:32:12 -0400

    moved to functions

--- trius_cam.py
+++ trius_cam.py
@@ -33,91 +33,106 @@ class IndiClient(PyIndi.BaseClient):
         pass
     def serverDisconnected(self, code):
         pass
-    
-# connect the server
-indiclient=IndiClient()
-indiclient.setServer("localhost",7624)
- 
-if (not(indiclient.connectServer())):
-     print("No indiserver running on "+indiclient.getHost()+":"+str(indiclient.getPort())+" - Try to run")
-     print("  indiserver indi_sx_ccd")
-     sys.exit(1)
+
+def connect_to_indi():
+    # connect the server
+    indiclient=IndiClient()
+    indiclient.setServer("localhost",7624)
      
-# Let's take some pictures
-ccd="SX CCD SXVR-H694"
-device_ccd=indiclient.getDevice(ccd)
-while not(device_ccd):
-    time.sleep(0.5)
+    if (not(indiclient.connectServer())):
+         print("No indiserver running on "+indiclient.getHost()+":"+str(indiclient.getPort())+" - Try to run")
+         print("  indiserver indi_sx_ccd")
+         sys.exit(1)
+
+    return indiclient
+
+def connect_to_ccd(indiclient)
+    # Let's take some pictures
+    ccd="SX CCD SXVR-H694"
     device_ccd=indiclient.getDevice(ccd)
-    print("Searching for device...")
+    while not(device_ccd):
+        time.sleep(0.5)
+        device_ccd=indiclient.getDevice(ccd)
+        print("Searching for device...")
 
-print("Found device")
- 
-ccd_connect=device_ccd.getSwitch("CONNECTION")
-while not(ccd_connect):
-    time.sleep(0.5)
+    print("Found device")
+     
     ccd_connect=device_ccd.getSwitch("CONNECTION")
-if not(device_ccd.isConnected()):
-    ccd_connect[0].s=PyIndi.ISS_ON  # the "CONNECT" switch
-    ccd_connect[1].s=PyIndi.ISS_OFF # the "DISCONNECT" switch
-    indiclient.sendNewSwitch(ccd_connect)
- 
-ccd_exposure=device_ccd.getNumber("CCD_EXPOSURE")
-while not(ccd_exposure):
-    time.sleep(0.5)
-    ccd_exposure=device_ccd.getNumber("CCD_EXPOSURE")
- 
-# Ensure the CCD simulator snoops the telescope simulator
-# otherwise you may not have a picture of vega
-ccd_active_devices=device_ccd.getText("ACTIVE_DEVICES")
-while not(ccd_active_devices):
-    time.sleep(0.5)
-    ccd_active_devices=device_ccd.getText("ACTIVE_DEVICES")
-ccd_active_devices[0].text="Telescope Simulator"
-indiclient.sendNewText(ccd_active_devices)
- 
-# we should inform the indi server that we want to receive the
-# "CCD1" blob from this device
-indiclient.setBLOBMode(PyIndi.B_ALSO, ccd, "CCD1")
- 
-ccd_ccd1=device_ccd.getBLOB("CCD1")
-while not(ccd_ccd1):
-    time.sleep(0.5)
-    ccd_ccd1=device_ccd.getBLOB("CCD1")
- 
-# a list of our exposure times
-exposures=[0.1, 0.5, 1.0, 1.5]
- 
-# we use here the threading.Event facility of Python
-# we define an event for newBlob event
-blobEvent=threading.Event()
-blobEvent.clear()
-i=0
-ccd_exposure[0].value=exposures[i]
-indiclient.sendNewNumber(ccd_exposure)
-while (i < len(exposures)):
-    name = str(exposures[i])
-    # wait for the ith exposure
-    blobEvent.wait()
-    # we can start immediately the next one
-    if (i + 1 < len(exposures)):
-        ccd_exposure[0].value=exposures[i+1]
-        blobEvent.clear()
-        indiclient.sendNewNumber(ccd_exposure)
-    # and meanwhile process the received one
-    for blob in ccd_ccd1:
-        print("name: ", blob.name," size: ", blob.size," format: ", blob.format)
-        # pyindi-client adds a getblobdata() method to IBLOB item
-        # for accessing the contents of the blob, which is a bytearray in Python
-        image=blob.getblobdata()
-        print("fits data type: ", type(image))
+    while not(ccd_connect):
+        time.sleep(0.5)
+        ccd_connect=device_ccd.getSwitch("CONNECTION")
+    if not(device_ccd.isConnected()):
+        ccd_connect[0].s=PyIndi.ISS_ON  # the "CONNECT" switch
+        ccd_connect[1].s=PyIndi.ISS_OFF # the "DISCONNECT" switch
+        indiclient.sendNewSwitch(ccd_connect)
 
-        image2D = np.array(image, dtype=np.byte)
-        
-        image2D.shape = (2200, 2750)
-        hdu = fits.PrimaryHDU()
-        hdu.data = image2D
-        hdu.writeto('/home/vncuser/Pictures/SX CCD/SX-CCD-Test-'+name+'.fits')
-    # and perform some computations while the ccd is exposing
-    # but this is outside the scope of this tutorial
-    i+=1
+ 
+    ccd_exposure=device_ccd.getNumber("CCD_EXPOSURE")
+    while not(ccd_exposure):
+        time.sleep(0.5)
+        ccd_exposure=device_ccd.getNumber("CCD_EXPOSURE")
+ 
+    # Ensure the CCD simulator snoops the telescope simulator
+    # otherwise you may not have a picture of vega
+    # ccd_active_devices=device_ccd.getText("ACTIVE_DEVICES")
+    # while not(ccd_active_devices):
+    #     time.sleep(0.5)
+    #     ccd_active_devices=device_ccd.getText("ACTIVE_DEVICES")
+    # ccd_active_devices[0].text="Telescope Simulator"
+    # indiclient.sendNewText(ccd_active_devices)
+ 
+    # we should inform the indi server that we want to receive the
+    # "CCD1" blob from this device
+    indiclient.setBLOBMode(PyIndi.B_ALSO, ccd, "CCD1")
+ 
+    ccd_ccd1=device_ccd.getBLOB("CCD1")
+    while not(ccd_ccd1):
+        time.sleep(0.5)
+        ccd_ccd1=device_ccd.getBLOB("CCD1")
+
+    return ccd_exposure, ccd_ccd1
+
+def exposure(indiclient, ccd_exposure, ccd_ccd1, exposures):
+ 
+    # we use here the threading.Event facility of Python
+    # we define an event for newBlob event
+    blobEvent=threading.Event()
+    blobEvent.clear()
+    i=0
+    ccd_exposure[0].value=exposures[i]
+    indiclient.sendNewNumber(ccd_exposure)
+    while (i < len(exposures)):
+        name = str(exposures[i])
+        # wait for the ith exposure
+        blobEvent.wait()
+        # we can start immediately the next one
+        if (i + 1 < len(exposures)):
+            ccd_exposure[0].value=exposures[i+1]
+            blobEvent.clear()
+            indiclient.sendNewNumber(ccd_exposure)
+        # and meanwhile process the received one
+        for blob in ccd_ccd1:
+            print("name: ", blob.name," size: ", blob.size," format: ", blob.format)
+            # pyindi-client adds a getblobdata() method to IBLOB item
+            # for accessing the contents of the blob, which is a bytearray in Python
+            image_data=blob.getblobdata()
+            print("fits data type: ", type(image_data))
+
+            image2D = blob.processblob()
+            
+            #image2D.shape = (2200, 2750)
+            hdu = fits.PrimaryHDU()
+            hdu.data = image2D
+            hdu.writeto('/home/vncuser/Pictures/SX CCD/SX-CCD-Test-'+name+'.fits')
+        # and perform some computations while the ccd is exposing
+        # but this is outside the scope of this tutorial
+        i+=1
+
+if __name__ == "__main__":
+    indiclient = connect_to_indi()
+    ccd_exposure, ccd_ccd1 = connect_to_ccd(indiclient)
+    
+    # a list of our exposure times
+    exposures=[0.1, 0.5, 1.0, 1.5]
+
+    exposure(indiclient, ccd_exposure, ccd_ccd1, exposures)
